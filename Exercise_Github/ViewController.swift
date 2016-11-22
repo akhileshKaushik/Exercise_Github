d7708257dc910a2d871aff3d95501e7ea968b0be
akhileshKaushik <akhilesh.kaushik90@gmail.com>
2016-11-22 13:45:05 +0530

    Initial Commit

---
+++ Exercise_Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise_Github
+//
+//  Created by Akhilesh Kaushik on 22/11/16.
+//  Copyright © 2016 Akhilesh Kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
